da11e24fa9cecbc7d71386de4dc0cc7e9f3789dd
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-12-04 15:51:05 +0300

    add universe checking for args and kwargs

--- task1/solution.py
+++ task1/solution.py
@@ -6,10 +6,12 @@ def strict(func) -> Callable:
     @functools.wraps(func)
     def wrapped(*args, **kwargs) -> Callable:
         annotations = func.__annotations__
+        #  делаем арги кваргами и все в общий словарь для общей проверки
+        all_kwargs = {**dict(zip(annotations.keys(), args)), **kwargs}
 
-        for count, (arg_name, expected_type) in enumerate(annotations.items()):
-            if count < len(args):
-                if not isinstance(args[count], expected_type):
+        for arg_name, expected_type in annotations.items():
+            if arg_name in all_kwargs:
+                if not isinstance(all_kwargs[arg_name], expected_type):
                     raise TypeError(f"'{arg_name}' не соответствует типу {expected_type.__name__}")
 
         result = func(*args, **kwargs)
@@ -25,3 +27,5 @@ def sum_two(a: int, b: int) -> int:
 if __name__ == "__main__":
     print(sum_two(1, 2))  # >>> 3
     print(sum_two(1, 2.4))  # >>> TypeError
+    print(sum_two(a=1, b=2))
+    print(sum_two(a=1, b=2.4))  # >>> TypeError
